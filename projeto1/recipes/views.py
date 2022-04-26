ca1dccc160f985244a8d22b4a9208b08bce3e455
joaomarcus13 <joaomarcus13@hotmail.com>
2022-04-25 22:29:43 -0300

    templates

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'joao'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'joao'})
