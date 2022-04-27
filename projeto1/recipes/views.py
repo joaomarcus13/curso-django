850258ca4bce8412d0da35d051f325a7b7bddf79
joaomarcus13 <joaomarcus13@hotmail.com>
2022-04-27 18:16:23 -0300

    blocks and extends templates

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -5,4 +5,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'joao'})
+    return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):
+    print(id)
+    return render(request, 'recipes/pages/recipe-view.html')
