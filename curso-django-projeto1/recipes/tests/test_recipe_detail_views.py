050f6e86fd10738a85f5d7fc1e56a4ec78ac610d
joaomarcus13 <joaomarcus13@hotmail.com>
2022-08-31 17:55:21 -0300

    first commit

---
+++ curso-django-projeto1/recipes/tests/test_recipe_detail_views.py
@@ -0,0 +1,19 @@
+
+from django.urls import resolve, reverse
+from recipes import views
+from recipes.tests.test_recipe_base import RecipeTestBase
+
+
+class RecipeDetailsViewsTest(RecipeTestBase):
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
